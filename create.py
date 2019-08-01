36be5715b1388372ce2ce84a23c1743e50ecd0cd
Nick Cattley <16906343+NickCattley@users.noreply.github.com>
2019-08-01 10:30:53 +0100

    Update create.py
    Added the GNU General Public License v3.0 as the default license for any project created via the script.

--- create.py
+++ create.py
@@ -10,7 +10,7 @@ path = f"{desktop}\\Python\\"
 def InitializeProject():
     os.makedirs(path + str(sys.argv[1]))
     user = Github("YOUR TOKEN HERE").get_user()
-    user.create_repo(sys.argv[1])
+    user.create_repo(name=sys.argv[1], license_template="gpl-3.0")
 
 
 if __name__ == "__main__":
